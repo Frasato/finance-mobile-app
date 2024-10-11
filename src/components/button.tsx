fdd601c66371158b3edbe843b24fb277d13adb37
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 15:26:41 -0300

    create style from button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,11 @@
 import { Text } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { ButtonProps } from "../@types/buttonProps";
 
-export default function Button(){
+export default function Button(props: ButtonProps){
     return(
-        <TouchableOpacity>
-            <Text>Login</Text>
+        <TouchableOpacity style={{paddingHorizontal: 90, paddingVertical: 10, borderRadius: 10, backgroundColor: props.color}}>
+            <Text>{props.label}</Text>
         </TouchableOpacity>
     )
 }
