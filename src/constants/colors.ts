cb1ec191323b31094a33d609c89b855162b09c70
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 10:58:11 -0300

    create a consts colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+const darkBlue: string = "#253659";
+const darkAquaGrenn: string = "#03A696";
+const lightAquaGreen: string = "#04BF9D";
+const lightOrange: string = "#F27457";
+const darkOrange: string = "#BF665E";
+
+export {
+    darkBlue,
+    darkAquaGrenn,
+    lightAquaGreen,
+    lightOrange,
+    darkOrange
+}
