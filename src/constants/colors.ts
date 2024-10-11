f72e428baae2bbfb790dee33432ac3c166d22f2d
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 12:28:41 -0300

    Add another color

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,4 @@
+const white: string = "#e8e6e6";
 const darkBlue: string = "#253659";
 const deepDarkBlue: string = "#131e36";
 const darkAquaGrenn: string = "#03A696";
@@ -6,6 +7,7 @@ const lightOrange: string = "#F27457";
 const darkOrange: string = "#BF665E";
 
 export {
+    white,
     darkBlue,
     deepDarkBlue,
     darkAquaGrenn,
