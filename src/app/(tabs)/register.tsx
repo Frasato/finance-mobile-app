b8ff3b85cffe6220a0341936f44faa0c2fbd9d8f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 16:29:59 -0300

    Add components to register page

--- src/app/(tabs)/register.tsx
+++ src/app/(tabs)/register.tsx
@@ -1,10 +1,18 @@
 import { View, Image, StyleSheet } from "react-native";
 import * as colors from "../../constants/colors";
+import InputText from "@/src/components/input";
+import Button from "@/src/components/button";
+import TextButton from "@/src/components/textButton";
 
 export default function Register(){
     return(
         <View style={styles.registerContainer}>
             <Image source={require('../../assets/register.png')} style={styles.image}/>
+            <InputText icon="person" placeholderText="Your Username"/>
+            <InputText icon="email" placeholderText="youremail@email.com"/>
+            <InputText icon="lock" placeholderText="Set a password"/>
+            <Button label="Register" color={colors.lightAquaGreen}/>
+            <TextButton label="Already have a account?" onPress={()=>{}}/>
         </View>
     )
 }
