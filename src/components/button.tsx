ef2e320d488a2462b8fb391fae519bf972f5a78b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 12:46:25 -0300

    Create Button Component

---
+++ src/components/button.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default function Button(){
+    return(
+        <TouchableOpacity>
+            <Text>Login</Text>
+        </TouchableOpacity>
+    )
+}
