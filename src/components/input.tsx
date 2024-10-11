611f9c467c333486358856055d5be502c33be089
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 12:29:17 -0300

    Add input and icon

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,9 +1,17 @@
-import { View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TextInput, View } from "react-native";
+import { InputProps } from "../@types/inputProps";
+import * as colors from "../constants/colors";
 
-export default function InputText(){
+export default function InputText(props: InputProps){
     return(
         <View>
-            
+            <MaterialIcons name={props.icon} size={20} color={colors.darkAquaGrenn}/>
+            <TextInput 
+                placeholder={props.placeholderText}
+                placeholderTextColor={colors.white}
+                cursorColor={colors.lightOrange}
+            />
         </View>
     )
 }
