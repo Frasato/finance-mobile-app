b851c1c5961cea3598ec4c8b736725d106fe0eb8
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 15:45:36 -0300

    Create Style for button

--- src/components/textButton.tsx
+++ src/components/textButton.tsx
@@ -1,10 +1,21 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 import { TextButtonProps } from "../@types/TextButtonProps";
+import * as colors from "../constants/colors";
 
 export default function TextButton(props: TextButtonProps){
     return(
         <TouchableOpacity>
-            <Text>{props.label}</Text>
+            <Text style={styles.text}>{props.label}</Text>
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    text: {
+        marginTop: 10,
+        color: colors.white,
+        textDecorationLine: 'underline',
+        textDecorationStyle: 'solid',
+        textDecorationColor: colors.white
+    }
+})
