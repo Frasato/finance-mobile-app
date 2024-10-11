8192697a222cf434a0b9d4d1f8d14395381990b9
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 16:24:58 -0300

    Add image and styles for it

--- src/app/(tabs)/register.tsx
+++ src/app/(tabs)/register.tsx
@@ -1,10 +1,10 @@
-import { View, StyleSheet } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import * as colors from "../../constants/colors";
 
 export default function Register(){
     return(
         <View style={styles.registerContainer}>
-            
+            <Image source={require('../../assets/register.png')} style={styles.image}/>
         </View>
     )
 }
@@ -15,5 +15,10 @@ const styles = StyleSheet.create({
         backgroundColor: colors.deepDarkBlue,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    image: {
+        width: 70 * 3,
+        height: 50 * 3,
+        marginBottom: 15,
+    },
 })
