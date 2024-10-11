aac07ba63423c3005a502333ca7bae20c678f338
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 10:58:29 -0300

    Change tabs style

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,26 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import * as colors from "../../constants/colors";
 
 export default function TabsLayouts(){
     return(
-        <Tabs>
-            <Tabs.Screen name="index"/>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: colors.darkBlue,
+                    borderTopWidth: 0,
+                },
+                tabBarActiveTintColor: colors.lightOrange,
+                tabBarInactiveTintColor: colors.darkOrange,
+                tabBarShowLabel: false
+            }}
+        >
+            <Tabs.Screen name="index" options={{
+                tabBarIcon: ({size, color}) => (
+                    <MaterialIcons name="home" size={size} color={color}/>
+                )
+            }}/>
         </Tabs>
     )
 }
