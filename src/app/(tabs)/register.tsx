60daaaa9b25b536514f1ed43597901c20a3d55aa
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 16:41:19 -0300

    Create a back to login page

--- src/app/(tabs)/register.tsx
+++ src/app/(tabs)/register.tsx
@@ -3,8 +3,16 @@ import * as colors from "../../constants/colors";
 import InputText from "@/src/components/input";
 import Button from "@/src/components/button";
 import TextButton from "@/src/components/textButton";
+import { useRouter } from "expo-router";
 
 export default function Register(){
+
+    const router = useRouter();
+
+    const redirectLogin = () =>{
+        router.back();
+    }
+
     return(
         <View style={styles.registerContainer}>
             <Image source={require('../../assets/register.png')} style={styles.image}/>
@@ -12,7 +20,7 @@ export default function Register(){
             <InputText icon="email" placeholderText="youremail@email.com"/>
             <InputText icon="lock" placeholderText="Set a password"/>
             <Button label="Register" color={colors.lightAquaGreen}/>
-            <TextButton label="Already have a account?" onPress={()=>{}}/>
+            <TextButton label="Already have an account?" onPress={redirectLogin}/>
         </View>
     )
 }
