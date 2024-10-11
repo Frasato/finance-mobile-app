745299888732c2de1f7b19f66f65436938003c05
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 16:21:18 -0300

    Add base styles for page

--- src/app/(tabs)/register.tsx
+++ src/app/(tabs)/register.tsx
@@ -1,9 +1,19 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import * as colors from "../../constants/colors";
 
 export default function Register(){
     return(
-        <View>
+        <View style={styles.registerContainer}>
             
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    registerContainer: {
+        flex: 1,
+        backgroundColor: colors.deepDarkBlue,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
