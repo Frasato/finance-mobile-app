b2eab24b1e3fbb9e1973555e12ef5f7f4a5b4450
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 16:17:16 -0300

    Add logic to redirect to register page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,15 +3,23 @@ import * as colors from "../../constants/colors";
 import InputText from "@/src/components/input";
 import Button from "@/src/components/button";
 import TextButton from "@/src/components/textButton";
+import { useRouter } from "expo-router";
 
 export default function Login(){
+
+    const router = useRouter();
+
+    const registerRedirect = () =>{
+        router.push("/register");
+    }
+
     return(
         <View style={styles.homeContainer}>
             <Image source={require('../../assets/login.png')} style={styles.imageLogin}/>
             <InputText icon="email" placeholderText="financeapp@email.com"/>
-            <InputText icon="lock" placeholderText="Senha"/>
+            <InputText icon="lock" placeholderText="Password"/>
             <Button color={colors.lightAquaGreen} label="Login"/>
-            <TextButton label="Register"/>
+            <TextButton label="Register" onPress={registerRedirect}/>
         </View>
     )
 }
