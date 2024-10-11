d8c517f234de4b0d8bb0ec64ed8cbad75c56b476
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 11:22:44 -0300

    Hidden the tab on login screen

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -5,22 +5,19 @@ import * as colors from "../../constants/colors";
 export default function TabsLayouts(){
     return(
         <Tabs
-            screenOptions={{
+            screenOptions={({route}) => ({
                 headerShown: false,
                 tabBarStyle: {
                     backgroundColor: colors.darkBlue,
                     borderTopWidth: 0,
+                    display: route.name === "index"? 'none' : 'flex',
                 },
                 tabBarActiveTintColor: colors.lightOrange,
                 tabBarInactiveTintColor: colors.darkOrange,
                 tabBarShowLabel: false
-            }}
+            })}
         >
-            <Tabs.Screen name="index" options={{
-                tabBarIcon: ({size, color}) => (
-                    <MaterialIcons name="home" size={size} color={color}/>
-                )
-            }}/>
+            <Tabs.Screen name="index" options={{href: null}}/>
         </Tabs>
     )
 }
