bc85e388062dda00951ff77d058afc50c5c364d0
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 11:06:20 -0300

    Create Style on main view

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import * as colors from "../../constants/colors";
 
 export default function Home(){
     return(
-        <View>
+        <View style={styles.homeContainer}>
             <Text>Home</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    homeContainer: {
+        flex: 1,
+        backgroundColor: colors.deepDarkBlue,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
