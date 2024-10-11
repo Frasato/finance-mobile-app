f111a974324ee9801d43196a2ab3a885210b6df3
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-11 11:06:02 -0300

    Add new color

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,4 +1,5 @@
 const darkBlue: string = "#253659";
+const deepDarkBlue: string = "#131e36";
 const darkAquaGrenn: string = "#03A696";
 const lightAquaGreen: string = "#04BF9D";
 const lightOrange: string = "#F27457";
@@ -6,6 +7,7 @@ const darkOrange: string = "#BF665E";
 
 export {
     darkBlue,
+    deepDarkBlue,
     darkAquaGrenn,
     lightAquaGreen,
     lightOrange,
