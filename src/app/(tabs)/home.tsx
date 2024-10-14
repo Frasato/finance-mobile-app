a9e9f42502457f6fdb68ed46e81e967fe59a2013
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 09:29:26 -0300

    Add some styles

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import * as colors from "../../constants/colors";
 
 export default function Home(){
     return(
-        <View>
-            <Text>Home</Text>
+        <View style={styles.homeContainer}>
+            <Text style={{color: 'white'}}>Home</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    homeContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.deepDarkBlue
+    },
+})
