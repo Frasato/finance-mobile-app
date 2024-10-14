c98019b7ab51fee98fabf6f8bbd2d1c9a0b979e0
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 08:56:15 -0300

    Create functions to verify empty fields and redirect

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,21 +4,41 @@ import InputText from "@/src/components/input";
 import Button from "@/src/components/button";
 import TextButton from "@/src/components/textButton";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function Login(){
 
     const router = useRouter();
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
     const registerRedirect = () =>{
         router.push("/register");
     }
 
+    const handlerEmail = (text: string) =>{
+        if(text.length > 0){
+            setEmail(text);
+        }
+    }
+    const handlerPassword = (text: string) =>{
+        if(text.length > 0){
+            setPassword(text);
+        }
+    }
+
+    const handlerLogin = () =>{
+        if(email?.length > 0 && password?.length > 0){
+            router.replace("/(tabs)/home");
+        }
+    }
+
     return(
         <View style={styles.homeContainer}>
             <Image source={require('../../assets/login.png')} style={styles.imageLogin}/>
-            <InputText icon="email" placeholderText="financeapp@email.com"/>
-            <InputText icon="lock" placeholderText="Password" secureTextEntry/>
-            <Button color={colors.lightAquaGreen} label="Login"/>
+            <InputText icon="email" placeholderText="financeapp@email.com" onChangeText={(text) => handlerEmail(text)}/>
+            <InputText icon="lock" placeholderText="Password" onChangeText={(text) => handlerPassword(text)} secureTextEntry/>
+            <Button color={colors.lightAquaGreen} label="Login" onPress={handlerLogin}/>
             <TextButton label="Register" onPress={registerRedirect}/>
         </View>
     )
