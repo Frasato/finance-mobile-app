c97887155002f39cc936b9ee1f59a5e29f7318ee
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 09:38:22 -0300

    Create money info component

---
+++ src/components/moneyInfo.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { MoneyInfosProps } from "../@types/moneyInfosProps";
+
+export default function MoneyInfo(props: MoneyInfosProps){
+    return(
+        <View>
+
+        </View>
+    );
+}
