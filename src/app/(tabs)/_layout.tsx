913214950a8767fa8e614eac65e0ef206aeeb69c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 09:33:03 -0300

    Fix some codes

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -6,31 +6,27 @@ import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colo
 export default function TabsLayouts(){
     return(
         <Tabs
-            screenOptions={({route}) => ({
+            screenOptions={{
                 headerShown: false,
                 tabBarStyle: {
                     backgroundColor: colors.darkBlue,
                     borderTopWidth: 0,
-                    display: route.name === "index" || "register"? "none" : "flex",
                 },
                 tabBarActiveTintColor: colors.lightOrange,
                 tabBarInactiveTintColor: colors.darkOrange,
                 tabBarShowLabel: false
-            })}
+            }}
         >
             <Tabs.Screen name="index" options={{
                 href: null,
-                tabBarStyle: {display: 'none'},
             }} />
             <Tabs.Screen name="register" options={{
                 href: null,
-                tabBarStyle: {display: 'none'},
             }} />
             <Tabs.Screen name="home" options={{
                 tabBarIcon: ({size, color}) => {
                     return <MaterialIcons name="home" size={size} color={color} />
                 },
-                tabBarStyle: {display: 'flex'}
             }}/>
         </Tabs>
     )
