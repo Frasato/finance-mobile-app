2988756792731fef88763ea93495d944749631b7
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 16:24:28 -0300

    Create structure of component

--- src/components/promotionCard.tsx
+++ src/components/promotionCard.tsx
@@ -1,9 +1,18 @@
-import { View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Image, Text, View, StyleSheet } from "react-native";
+import * as colors from "../constants/colors";
 
 export default function PromotionCard(){
     return(
         <View>
-            
+            <Image source={require("../assets/family.jpg")}/>
+            <View>
+                <View>
+                    <MaterialIcons name="attach-money" size={30} color={colors.white}/>
+                    <Text>Loan available</Text>
+                </View>
+                <Text>$1700</Text>
+            </View>
         </View>
     )
 }
