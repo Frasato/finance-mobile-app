2d25cdb9718d929a2ac76cad8283b87beb0ef334
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 08:56:50 -0300

    Add on Chang Text type on interface

--- src/@types/inputProps.ts
+++ src/@types/inputProps.ts
@@ -3,5 +3,6 @@ import { IconNameProps } from "./iconNameProps";
 export interface InputProps {
     placeholderText: string,
     icon: IconNameProps,
-    secureTextEntry?: boolean
+    secureTextEntry?: boolean,
+    onChangeText: (text: string) => void 
 }
