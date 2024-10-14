6f1db3d60f486a5d9b0ce2baf83095bac72cf75f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 16:14:34 -0300

    Add items and style on component

--- src/components/cardItem.tsx
+++ src/components/cardItem.tsx
@@ -1,9 +1,29 @@
-import { View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, View, StyleSheet } from "react-native";
+import * as colors from "../constants/colors";
 
 export default function CardItem(){
     return(
-        <View>
-            
+        <View style={styles.cardItemContainer}>
+            <MaterialIcons name="airplane-ticket" size={30} color={colors.white} />
+            <Text style={styles.title}>Ticket Airplane</Text>
+            <Text style={styles.value}>$300</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    cardItemContainer: {
+        marginRight: 10,
+        backgroundColor: colors.darkBlue,
+        padding: 10,
+        height: 150,
+        borderRadius: 10,
+        justifyContent: 'space-around'
+    },
+    title: {color: colors.grey},
+    value: {
+        color: colors.grey,
+        fontSize: 30,
+    }
+});
