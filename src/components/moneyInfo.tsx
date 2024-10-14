a5c95e1429c5a37a66502f76d6eb5184eba952f0
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 10:28:22 -0300

    Stylize money component

--- src/components/moneyInfo.tsx
+++ src/components/moneyInfo.tsx
@@ -1,10 +1,23 @@
-import { View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { MoneyInfosProps } from "../@types/moneyInfosProps";
+import * as colors from "../constants/colors";
 
 export default function MoneyInfo(props: MoneyInfosProps){
     return(
-        <View>
-
+        <View style={styles.moneyContainer}>
+            <Text style={{color: colors.white, fontSize: 20}}>{props.label}:</Text>
+            <Text style={{color: props.valueColor, fontSize: 30, fontWeight: 'bold'}}>R${props.value}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    moneyContainer: {
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        width: '100%',
+        borderBottomWidth: 1,
+        borderStyle: 'solid',
+        borderBottomColor: colors.white,
+    }
+})
