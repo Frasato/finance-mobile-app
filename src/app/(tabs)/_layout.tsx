79229b2bcb8044e3a5ee27d9e375f8920c64f663
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 09:29:46 -0300

    Hideng icons on tab bar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import * as colors from "../../constants/colors";
+import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 export default function TabsLayouts(){
     return(
@@ -10,14 +11,27 @@ export default function TabsLayouts(){
                 tabBarStyle: {
                     backgroundColor: colors.darkBlue,
                     borderTopWidth: 0,
-                    display: route.name === "index" || "register" ? 'none' : 'flex',
+                    display: route.name === "index" || "register"? "none" : "flex",
                 },
                 tabBarActiveTintColor: colors.lightOrange,
                 tabBarInactiveTintColor: colors.darkOrange,
                 tabBarShowLabel: false
             })}
         >
-            <Tabs.Screen name="home" options={{tabBarStyle: {display: 'flex'}}}/>
+            <Tabs.Screen name="index" options={{
+                href: null,
+                tabBarStyle: {display: 'none'},
+            }} />
+            <Tabs.Screen name="register" options={{
+                href: null,
+                tabBarStyle: {display: 'none'},
+            }} />
+            <Tabs.Screen name="home" options={{
+                tabBarIcon: ({size, color}) => {
+                    return <MaterialIcons name="home" size={size} color={color} />
+                },
+                tabBarStyle: {display: 'flex'}
+            }}/>
         </Tabs>
     )
 }
