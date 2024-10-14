38f03f0473d965ccf180383d41742762ecb18452
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-14 08:55:54 -0300

    Add onpress

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,7 +4,7 @@ import { ButtonProps } from "../@types/buttonProps";
 
 export default function Button(props: ButtonProps){
     return(
-        <TouchableOpacity style={{paddingHorizontal: 90, paddingVertical: 10, borderRadius: 10, backgroundColor: props.color}}>
+        <TouchableOpacity onPress={props.onPress} style={{paddingHorizontal: 90, paddingVertical: 10, borderRadius: 10, backgroundColor: props.color}}>
             <Text>{props.label}</Text>
         </TouchableOpacity>
     )
