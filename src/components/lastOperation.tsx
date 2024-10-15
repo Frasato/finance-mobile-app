f17c1c3d87b71147838705f1cf734022bb8a1aa6
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-15 11:05:17 -0300

    Add the props type

--- src/components/lastOperation.tsx
+++ src/components/lastOperation.tsx
@@ -1,21 +1,22 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { Text, View, StyleSheet } from "react-native";
 import * as colors from "../constants/colors";
+import { lastOperationProps } from "../@types/lastOperationProps";
 
-export default function LastOperation(){
+export default function LastOperation({category, date, value, icon}: lastOperationProps){
     return(
         <View style={styles.container}>
             <View style={styles.infosContainer}>
-                <MaterialIcons name="arrow-back" size={20} color={colors.white} />
-                <Text style={styles.text}>Hamburguer</Text>
+                <MaterialIcons name={icon} size={20} color={colors.white} />
+                <Text style={styles.text}>{category}</Text>
             </View>
             <View style={styles.infosContainer}>
                 <MaterialIcons name="calendar-month" size={20} color={colors.white} />
-                <Text style={styles.text}>10/10/2024</Text>
+                <Text style={styles.text}>{date}</Text>
             </View>
             <View style={styles.infosContainer}>
                 <MaterialIcons name="attach-money" size={20} color={colors.white} />
-                <Text style={styles.text}>50,00</Text>
+                <Text style={styles.text}>{value}</Text>
             </View>
         </View>
     )
