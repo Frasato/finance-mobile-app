83e4bc4481ef9f2468816406b88ef5040b5d6413
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-15 10:56:00 -0300

    Create a Scroll view

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -4,25 +4,28 @@ import MoneyInfo from "@/src/components/moneyInfo";
 import LastOperation from "@/src/components/lastOperation";
 import Card from "@/src/components/card";
 import PromotionCard from "@/src/components/promotionCard";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Home(){
     return(
-        <View style={styles.homeContainer}>
-            <MoneyInfo label="Entries" value={1300.00} valueColor={colors.lightAquaGreen} icon="attach-money"/>
-            <MoneyInfo label="Out's" value={100.00} valueColor={colors.white} icon="output"/>
-            <MoneyInfo label="Credit" value={50.00} valueColor={colors.grey} icon="credit-card"/>
-            <View>
-                <Text style={{color: colors.grey, fontSize: 20, marginBottom: 20, marginTop: 30}}>Last Operation:</Text>
-                <LastOperation />
-            </View>
-            <PromotionCard />
-            <View style={{marginTop: 10}}>
-                <Text style={{color: colors.grey, fontSize: 20, marginBottom: 20, marginTop: 20}}>Promotions:</Text>
-                <View style={{flexDirection: 'row'}}>
-                    <Card />
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.homeContainer}>
+                <MoneyInfo label="Entries" value={1300.00} valueColor={colors.lightAquaGreen} icon="attach-money"/>
+                <MoneyInfo label="Out's" value={100.00} valueColor={colors.white} icon="output"/>
+                <MoneyInfo label="Credit" value={50.00} valueColor={colors.grey} icon="credit-card"/>
+                <View>
+                    <Text style={{color: colors.grey, fontSize: 20, marginBottom: 20, marginTop: 30}}>Last Operation:</Text>
+                    <LastOperation />
+                </View>
+                <PromotionCard />
+                <View style={{marginTop: 10}}>
+                    <Text style={{color: colors.grey, fontSize: 20, marginBottom: 20, marginTop: 20}}>Promotions:</Text>
+                    <View style={{flexDirection: 'row'}}>
+                        <Card />
+                    </View>
                 </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
