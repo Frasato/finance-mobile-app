e20252dcc42e92ff83ce07bee3126ddca2b514ac
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-16 12:51:09 -0300

    Create the dropdown logic

--- src/app/(tabs)/entries.tsx
+++ src/app/(tabs)/entries.tsx
@@ -7,16 +7,30 @@ import * as colors from "../../constants/colors";
 export default function Entries(){
 
     const [value, setValue] = useState<number>(0);
+    const [category, setCategory] = useState<string>('');
 
     const handlerValue = (text: string) => {
         const valueNumber: number = Number(text);
         setValue(valueNumber);
     }
 
+    const handlerCategory = (text: any) => setCategory(text);
+
     return(
         <View>
             <InputText icon="attach-money" placeholderText="Value" onChangeText={(text) => handlerValue(text)}/>
-            <DropdownSelect label="" placeholder="" options={[]} selectedValue={} onValueChange={} primaryColor={colors.darkBlue}/>
+            <DropdownSelect 
+                label="Entries..."
+                placeholder="Select a category..."
+                options={[
+                    {title: 'Pay-day', data: 'pay-day'},
+                    {title: 'Sell', data: 'sell'},
+                    {title: 'Other', data: 'other'},
+                ]}
+                selectedValue={category}
+                onValueChange={(text) => handlerCategory(text)}
+                primaryColor={colors.darkBlue}
+            />
         </View>
     )
 }
