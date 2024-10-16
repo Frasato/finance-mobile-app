c9cc24b947e27ac0bd26ceb92242a48ddab4d718
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-16 12:32:44 -0300

    Add value input

--- src/app/(tabs)/entries.tsx
+++ src/app/(tabs)/entries.tsx
@@ -1,9 +1,10 @@
+import InputText from "@/src/components/input";
 import { View } from "react-native";
 
 export default function Entries(){
     return(
         <View>
-            
+            <InputText icon="attach-money" placeholderText="Value" onChangeText={handlerValue}/>
         </View>
     )
 }
