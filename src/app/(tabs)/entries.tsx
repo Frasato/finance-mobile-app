5d1617f92cddd16248c478edbe57e79e7d516449
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-16 12:34:58 -0300

    Add logic

--- src/app/(tabs)/entries.tsx
+++ src/app/(tabs)/entries.tsx
@@ -1,10 +1,19 @@
 import InputText from "@/src/components/input";
+import { useState } from "react";
 import { View } from "react-native";
 
 export default function Entries(){
+
+    const [value, setValue] = useState<number>(0);
+
+    const handlerValue = (text: string) => {
+        const valueNumber: number = Number(text);
+        setValue(valueNumber);
+    }
+
     return(
         <View>
-            <InputText icon="attach-money" placeholderText="Value" onChangeText={handlerValue}/>
+            <InputText icon="attach-money" placeholderText="Value" onChangeText={(text) => handlerValue(text)}/>
         </View>
     )
 }
