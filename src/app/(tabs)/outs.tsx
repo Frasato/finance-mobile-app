5d2e3716a56d63c50c0b16a21c6aa601eead590a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 10:00:37 -0300

    Import drop down inout

--- src/app/(tabs)/outs.tsx
+++ src/app/(tabs)/outs.tsx
@@ -1,11 +1,36 @@
 import InputText from "@/src/components/input";
 import { StyleSheet, View } from "react-native";
 import * as colors from "../../constants/colors";
+import DropdownSelect from "react-native-input-select";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Outs(){
     return(
         <View style={styles.container}>
             <InputText icon="money-off" placeholderText="Money out value..." onChangeText={()=>{}}/>
+            <InputText icon="question-mark" placeholderText="For what?" onChangeText={()=>{}}/>
+            <DropdownSelect
+                    dropdownContainerStyle={{paddingHorizontal: 27}}
+                    dropdownStyle={styles.inDropDown}
+                    placeholderStyle={{color: colors.white}}
+                    selectedItemStyle={{color: colors.white}}
+                    autoCloseOnSelect={true}
+                    dropdownIcon={
+                        <MaterialIcons name="keyboard-arrow-down" size={15} color={colors.white}/>
+                    }
+                    placeholder="Select a category..."
+                    options={[
+                        {label: 'Bills', value: 'bills'},
+                        {label: 'Food', value: 'food'},
+                        {label: 'Travel', value: 'travel'},
+                        {label: 'Video Strems', value: 'streams'},
+                        {label: 'Cats/Dogs', value: 'animals'},
+                        {label: 'Others', value: 'other'},
+                    ]}
+                    selectedValue={''}
+                    onValueChange={(text) => {}}
+                    primaryColor={colors.white}
+                />
         </View>
     )
 }
@@ -16,5 +41,9 @@ const styles = StyleSheet.create({
         backgroundColor: colors.deepDarkBlue,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    inDropDown: {
+        backgroundColor: colors.deepDarkBlue,
+        borderColor: colors.white
+    },
 })
