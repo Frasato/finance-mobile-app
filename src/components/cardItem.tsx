5898ad126b2e5c1301e6b14cd90d6e2b93603aa3
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 11:31:31 -0300

    Add props for the values

--- src/components/cardItem.tsx
+++ src/components/cardItem.tsx
@@ -1,13 +1,14 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { Text, View, StyleSheet } from "react-native";
 import * as colors from "../constants/colors";
+import { CardItemProps } from "../@types/cardItemProps";
 
-export default function CardItem(){
+export default function CardItem(props: CardItemProps){
     return(
         <View style={styles.cardItemContainer}>
-            <MaterialIcons name="airplane-ticket" size={30} color={colors.white} />
-            <Text style={styles.title}>Ticket Airplane</Text>
-            <Text style={styles.value}>$300</Text>
+            <MaterialIcons name={props.icon} size={30} color={colors.white} />
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.value}>${props.value}</Text>
         </View>
     )
 }
