096d09d8a70d4a4f2d6aae48803f0958aefe0daa
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 12:13:24 -0300

    Add logic to logout

--- src/app/(tabs)/logout.tsx
+++ src/app/(tabs)/logout.tsx
@@ -1,13 +1,26 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import * as colors from "../../constants/colors";
 import Button from "@/src/components/button";
+import { useContext } from "react";
+import { User } from "@/src/contexts/userContext";
+import { useRouter } from "expo-router";
 
 export default function Logout(){
+
+    const {setUserEmail, setUsernameContext} = useContext(User);
+    const router = useRouter();
+
+    const handlerLogout = () =>{
+        setUserEmail('');
+        setUsernameContext('');
+        router.replace("/(tabs)/");
+    }
+
     return(
         <View style={styles.container}>
             <Image source={require("../../assets/logout.png")} style={{width: 200, height: 200}}/>
             <Text style={styles.text}>Are you sure you whant to log out?</Text>
-            <Button color={colors.lightOrange} label="LogOut" onPress={()=>{}}/>
+            <Button color={colors.lightOrange} label="LogOut" onPress={handlerLogout}/>
         </View>
     )
 }
