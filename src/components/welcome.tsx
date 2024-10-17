a0be038d711f8867acfefd16341fcf3bd2936db6
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 10:55:15 -0300

    Add styles for container

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,14 +1,29 @@
 import { useContext } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { User } from "../contexts/userContext";
+import * as colors from "../constants/colors";
 
 export default function Welcome(){
 
     const {username} = useContext(User);
 
     return(
-        <View>
+        <View style={styles.container}>
             <Text>{username}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.white,
+        height: 200,
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'flex-start',
+        padding: 20,
+        borderBottomRightRadius: 40,
+        borderBottomLeftRadius: 40
+    }
+})
