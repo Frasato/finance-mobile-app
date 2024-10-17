4d9a35cef3cf6b3e92aabb62964b6c6b5e197ea7
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 15:36:12 -0300

    Create variables

--- src/contexts/lastOperationContext.tsx
+++ src/contexts/lastOperationContext.tsx
@@ -1,11 +1,35 @@
-import { createContext, ReactNode } from "react";
+import React, { createContext, ReactNode, useState } from "react";
 
-const OperationInfos = createContext();
+const OperationInfos = createContext<{
+    operationCategory: any;
+    operationValue: number;
+    date: any;
+    setOperationCategory: React.Dispatch<React.SetStateAction<any>>;
+    setDate: React.Dispatch<React.SetStateAction<any>>;
+    setOperationValue: React.Dispatch<React.SetStateAction<number>>;
+}>({
+    operationCategory: undefined,
+    operationValue: 0,
+    date: undefined,
+    setOperationCategory: () => {},
+    setDate: () => {},
+    setOperationValue: () => {}
+});
 
 const OperationInfosProvider = ({children} : {children: ReactNode}) =>{
+    
+    const [operationValue, setOperationValue] = useState<number>(0);
+    const [operationCategory, setOperationCategory] = useState();
+    const [date, setDate] = useState();
+    
     return(
         <OperationInfos.Provider value={{
-
+            operationCategory,
+            setOperationCategory,
+            operationValue,
+            setOperationValue,
+            date,
+            setDate,
         }}>
             {children}
         </OperationInfos.Provider>
