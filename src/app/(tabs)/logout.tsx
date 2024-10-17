ce4b072bfc3c668d069fa99b66c7256918e533c7
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 11:49:09 -0300

    Add styles

--- src/app/(tabs)/logout.tsx
+++ src/app/(tabs)/logout.tsx
@@ -1,9 +1,28 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import * as colors from "../../constants/colors";
+import Button from "@/src/components/button";
 
 export default function Logout(){
     return(
-        <View>
-            
+        <View style={styles.container}>
+            <Text style={styles.text}>Are you sure you whant to log out?</Text>
+            <Button color={colors.lightOrange} label="LogOut" onPress={()=>{}}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.deepDarkBlue,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+    text: {
+        color: colors.white,
+        fontSize: 15,
+        fontWeight: '400',
+        marginBottom: 20,
+    }
+})
