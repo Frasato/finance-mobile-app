83d154df85644fc96c17400616d6029083f7d7cd
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 11:31:19 -0300

    Create cards items

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,10 +5,11 @@ import CardItem from "./cardItem";
 export default function Card(){
     return(
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginBottom: 10}}>
-            <CardItem />
-            <CardItem />
-            <CardItem />
-            <CardItem />
+            <CardItem icon="airplane-ticket" title="Airplane Ticket" value={300}/>
+            <CardItem icon="house" title="Your house" value={1000}/>
+            <CardItem icon="yard" title="Build a Garden" value={150}/>
+            <CardItem icon="travel-explore" title="World Travel Tour" value={640}/>
+            <CardItem icon="computer" title="Your Own PC" value={50}/>
         </ScrollView>
     )
 }
