23180043c7c34bd9b2710d181f5176f98420506a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-17 09:56:10 -0300

    Add style for container

--- src/app/(tabs)/outs.tsx
+++ src/app/(tabs)/outs.tsx
@@ -1,9 +1,20 @@
-import { View } from "react-native";
+import InputText from "@/src/components/input";
+import { StyleSheet, View } from "react-native";
+import * as colors from "../../constants/colors";
 
 export default function Outs(){
     return(
-        <View>
-            
+        <View style={styles.container}>
+            <InputText icon="money-off" placeholderText="Money out value..." onChangeText={()=>{}}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.deepDarkBlue,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
