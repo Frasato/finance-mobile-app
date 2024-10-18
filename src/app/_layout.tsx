c45c7cbcdf431e55a3ff48fe1aaa2aecd3b67ee4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-18 15:50:28 -0300

    Import operation Provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,14 +2,17 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { Slot } from "expo-router";
 import UserProvider from "../contexts/userContext";
+import OperationInfosProvider from "../contexts/lastOperationContext";
 
 export default function Layout(){
     return(
         <UserProvider>
-            <GestureHandlerRootView>
-                <StatusBar style="light" />
-                <Slot />
-            </GestureHandlerRootView>
+            <OperationInfosProvider>
+                <GestureHandlerRootView>
+                    <StatusBar style="light" />
+                    <Slot />
+                </GestureHandlerRootView>
+            </OperationInfosProvider>
         </UserProvider>
     )
 }
